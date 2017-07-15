9fc9f0ce948224ca6c11d5c199b64d574525aa0e
landpack <ada.frank.ak@gmail.com>
2017-07-15 22:48:52 +0800

    [clear] the test code

--- pubsub.py
+++ pubsub.py
@@ -4,9 +4,9 @@ from functools import wraps
 class PubSub(object):
 
     @classmethod
-    def run(cls, target_function_list={}, ret=None, **public_kwargs):
+    def run(cls, target_function_list={}, ret=None, *input_args, **input_kwargs):
         for f_name, func in target_function_list.items():
-            func(ret, **public_kwargs)
+            func(ret, *input_args, **input_kwargs)
 
     @classmethod
     def pub(cls, name, condition=lambda *args, **kwargs: True):
@@ -20,7 +20,7 @@ class PubSub(object):
 
                 ret = f(*args, **kwargs)
                 if condition(*args, **kwargs):
-                    cls.run(target_f, ret, **kwargs)
+                    cls.run(target_f, ret, *args, **kwargs)
                 return ret
             return __wrapper
         return _wrapper
@@ -29,9 +29,7 @@ class PubSub(object):
     def sub(cls, name):
         target_f = getattr(cls, name, None)
         if target_f == None:
-            print 'com here'*10
             setattr(cls, name, {})
-            print getattr(cls, name)
 
         def _wrapper(f):
             target_f = getattr(cls, name, None)
@@ -51,34 +49,50 @@ def my_condition(*args, **kwargs):
         return False
 
 
-
-@PubSub.sub('condition_channel')
-def after_con_hook(ret, **public_kwargs):
-    print '[x] query database for uid=123', public_kwargs
-
-@PubSub.pub('collection_channel')
-@PubSub.pub('notify_channel')
-@PubSub.pub('condition_channel', my_condition)
-def i_am_login(uid, age):
-    print 'login with ~~', uid, 'age', age
-    return age
+@PubSub.sub('with_condition_channel_1')
+def abserver_with_condition(ret, *input_args, **input_kwargs):
+    """
+    If my condition is true, I care about that information!
+    else ignore me!
+    """
+    print '[ S ] query database for uid=123', public_kwargs
 
 
-@PubSub.sub('notify_channel')
-def notify_everyone(ret, **public_kwargs):
-    print '[P] a new one login with', public_kwargs
+@PubSub.pub('without_condition_channel_3')
+@PubSub.pub('without_condition_channel_2')
+@PubSub.pub('with_condition_channel_1', my_condition)
+def user_login_api(*args, **kwargs):
+    """
+    if you like to call function with name the params, as below code show
+        user_login_api(1234, 25)
+    you should read the params by access the args tuple, as below code show
+        uid, age = args
+    How to define this function is up to you!
+    """
+    age = kwargs.get('age')
+    uid = kwargs.get('uid')
+
+    age = age if age else args[1]
+    uid = uid if uid else args[0]
+
+    print 'login with uid={} age={}'.format(uid, age)
+    return kwargs.get('uid')
 
 
-@PubSub.sub('notify_channel')
-def notify_everyone_2(ret, **public_kwargs):
-    print '[P-2] a new one login with', public_kwargs
+@PubSub.sub('without_condition_channel_2')
+def abserver_without_condition(ret, *input_args, **input_kwargs):
+    print '[ A ] a new user login with', input_kwargs
 
 
-@PubSub.sub('collection_channel')
-def notify_everyone(ret, **public_kwargs):
-    print '[C] a new one login with, collection return value', ret
+@PubSub.sub('without_condition_channel_2')
+def abserver_without_condition(ret, *input_args, **input_kwargs):
+    print '[ A ] a new one login with', input_args
 
+
+@PubSub.sub('without_condition_channel_3')
+def abserver_without_condition(ret, *input_args, **input_kwargs):
+    print '[ C ] a new one login with, collection return value', ret
 
 if __name__ == '__main__':
-    i_am_login('123', age=29)
-    i_am_login(uid='833', age=19)
+    user_login_api('123', age=29)  # the both `args` and `kwargs`
+    user_login_api(uid='833', age=19)  # only `kargs`
